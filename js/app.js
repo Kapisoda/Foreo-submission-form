2c4e6256dd586f2b0b364df18984aa99bce2146c
Filip Kapusta <filipkapusta1@gmail.com>
2018-04-26 10:55:51 +0200

    Validations in jQuery

--- js/app.js
+++ js/app.js
@@ -2,64 +2,77 @@ $(document).foundation()
 
 
 $(document).ready(function () {
-    $('input').blur(function () {
-        if ($('#password').val() == $('#confirmPassword').val() && $('#confirmPassword').val() && $('#password').val()) {
-            $("input[type=password]").removeClass("input-error").addClass("lightsaber-input");
-            $("#spanPassword").empty();
-            return false;
-        } else if ($('#password').val() != $('#confirmPassword').val()) {
-            $("input[type=password]").addClass("input-error");
-            $("#spanPassword").text('Password does not match the confirm password');
-        }
-        $('#name, #email, #password, #confirmPassword').each(function () {
-            if ($(this).val() != '') {
-                $(this).removeClass("input-error").addClass("lightsaber-input");
-
-            }
-        });
-
-    });
 
     $("#button-submit").click(function () {
+        var flag = false;
+
+        $("span").each(function (index) {
+            $(this).empty();
+        });
+
+        $("input").each(function (index) {
+            $(this).removeClass("input-error");
+        });
+
+
         if (!$('#name').val()) {
             $("#spanName").text('Name is empty');
             $("#name").addClass("input-error");
-        } else {
-            $("#name").removeClass("input-error");
-            $('#spanName').empty();
+            flag = true;
         }
+
         var email = $("#email").val();
         if (!isValidEmailAddress(email)) {
             $("#spanEmail").text('Email is not valid');
             $("#email").addClass("input-error");
-        } else {
-            $("#email").removeClass("input-error");
-            $('#spanName').empty();
+            flag = true;
         }
+
         if (!$('#password').val()) {
             $("#spanPassword").text('Password is empty');
             $("#password").addClass("input-error");
-        } else {
-            $("#password").removeClass("input-error");
-            $("#spanPassword").empty();
+            flag = true;
         }
+
         if (!$('#confirmPassword').val()) {
             $("#spanConfirmPassword").text('Confirm password is empty');
             $("#confirmPassword").addClass("input-error");
-        } else {
-            $("#password").removeClass("input-error");
-            $("#spanConfirmPassword").empty();
+            flag = true;
         }
+
         if ($('#password').val() != $('#confirmPassword').val()) {
-            $("form").submit(function (e) {
-                e.preventDefault();
-            });
             $("input[type=password]").addClass("input-error");
             $("#spanPassword").text('Password does not match the confirm password');
+            flag = true;
         }
 
+        if ($("#birthDate").val()) {
+            var inputDate = $("#birthDate").val();
+            var age = isValidBirthDate(inputDate);
+            if (age < 18) {
+                $("#spanBirthDate").text('Birth date is not valid');
+                $("#birthDate").addClass("input-error");
+                flag = true;
+            }
+        }
+        if ($("#phoneNumber").val()) {
+            var phone = $("#phoneNumber").val();
+            if (!isValidPhone(phone)) {
+                $("#spanPhoneNumber").text('Phone number is not valid');
+                $("#phoneNumber").addClass("input-error");
+                flag = true;
+            }
+        }
 
+        console.log(flag);
 
+        $("form").submit(function (e) {
+            if (flag == true) {
+                e.preventDefault();
+            } else {
+                this.submit();
+            }
+        });
 
     });
 
@@ -68,13 +81,20 @@ $(document).ready(function () {
         return pattern.test(emailAddress);
     };
 
-    /* $("#button-submit").click(function () {
-         $('#name, #email, #password, #confirmPassword').each(function () {
-             if ($(this).val() == '') {
-                 $(this).addClass("input-error");
-             } else {
-                 $(this).removeClass("input-error").addClass("lightsaber-input");
-             }
-         });
-     }); */
+    function isValidBirthDate(DOB) {
+        var today = new Date();
+        var birthDate = new Date(DOB);
+        var age = today.getFullYear() - birthDate.getFullYear();
+        var m = today.getMonth() - birthDate.getMonth();
+        if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) {
+            age--;
+        }
+        return age;
+    }
+
+    function isValidPhone(txtPhone) {
+        var pattern = new RegExp(/^[0-9-+]+$/);
+        return pattern.test(txtPhone);
+    }
+
 });
